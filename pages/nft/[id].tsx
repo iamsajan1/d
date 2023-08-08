50ea01b6650907faa1b9f75c0c4f7191894c93cc
sajan <kumarsantosh.iict06a@gmail.com>
2023-08-09 00:58:06 +0530

    Updated pages/nft/[id].tsx

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -34,20 +34,20 @@ function NFTDropPage() {
         </div>
         </div>
         {/*Right*/}
-        <div className='flex-1 flex-col p-12 lg:col-span-6'>
+        <div className="flex-1 flex-col p-12 lg:col-span-6">
           {/*Header*/}
-          <header className='flex items-center justify-between'>
+          <header className="flex items-center justify-between">
             <h1 className="w-52 cursor-pointer text-xl font-extralight 
             sm:w-80">The
-            <span className="font-extrabold text-black "  style={{ textDecoration: 'underline', color: '#f095dc' }}> SAGOBULL </span>
+            <span className="font-extrabold text-black "  style={{ textDecoration: "underline", color: "#f095dc" }}> SAGOBULL </span>
               nft market place</h1>
-              <button onClick={()=>address?  disconnect(): connectWithMetamask()} className='rounded-full bg-pink-400 text-white px-4 py-2
-            text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>{address?'Sign Out': 'Sign in'}
+              <button onClick={()=>address?  disconnect(): connectWithMetamask()} className="rounded-full bg-pink-400 text-white px-4 py-2
+            text-xs font-bold lg:px-5 lg:py-3 lg:text-base">{address?"Sign Out": "Sign in"}
             </button>
           </header>
-          <hr className='my-2 border'></hr>
+          <hr className="my-2 border"></hr>
           {address&&(
-            <p className='text-center text-sm text-red-400'>
+            <p className="text-center text-sm text-red-400">
               you are logged in with wallet {address.substring(0,6)}....
               {address.substring(address.length-5)}
             </p>
@@ -56,17 +56,17 @@ function NFTDropPage() {
           <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center
           lg:space-y-0 lg:justify-center ">
 
-            <img  className='w-80 object-cover  lg:mb-10 lg:mt-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdUHImpzuUnuVcejhMEkRbYpqjpsQxtcil0g&usqp=CAU" 
+            <img  className="w-80 object-cover  lg:mb-10 lg:mt-10" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdUHImpzuUnuVcejhMEkRbYpqjpsQxtcil0g&usqp=CAU" 
             alt=""/>
-           <h1 className='text-3xl font-bold lg:text-5xl'>
+           <h1 className="text-3xl font-bold lg:text-5xl">
             The SAGOBULL nft club | NFT Drop
            </h1>
-           <p className='pt-2 text-xl text-green-400  '>
-            02/40 NFT's claimed
+           <p className="pt-2 text-xl text-green-400  ">
+            02/40 NFT"s claimed
            </p>
           </div>
           {/*mint button*/}
-        <button className='h-16 w-full bg-red-500 text-white rounded-full  mt-10  font-bold lg:mt-10'>
+        <button className="h-16 w-full bg-red-500 text-white rounded-full  mt-10  font-bold lg:mt-10">
           Mint NFT (0.02 ETH)
         </button>
       </div>
